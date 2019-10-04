cd555cd9f88911ac963a1b3d3b2cf0af7b07ffe5
Stephan Großberndt <s.grossberndt@sidebysite.de>
2019-10-04 10:42:05 +0200

    Initial code created by Android Studio

---
+++ app/src/main/java/com/example/crashtest/webviewcrash/ui/main/MainFragment.java
@@ -0,0 +1,37 @@
+package com.example.crashtest.webviewcrash.ui.main;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.crashtest.webviewcrash.R;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+
+public class MainFragment extends Fragment {
+
+	private MainViewModel mViewModel;
+
+	public static MainFragment newInstance() {
+		return new MainFragment();
+	}
+
+	@Nullable
+	@Override
+	public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+													 @Nullable Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.main_fragment, container, false);
+	}
+
+	@Override
+	public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+		// TODO: Use the ViewModel
+	}
+
+}
